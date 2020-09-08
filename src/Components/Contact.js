9cf903ddc752a240d14d929c4a7c2127a116ecc8
Cheria Artis <cheriaa34@gmail.com>
2020-09-08 11:00:13 -0400

    "Fixing errors"

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const Contact = ({ data }) => {
-   const [url, setUrl] = useState('mailto:cheria@cagraphicsllc.com?subject=subject&body=body');
    const [name, setName] = useState('');
    const [subject, setSubject] = useState('');
    const [email, setEmail] = useState('');
